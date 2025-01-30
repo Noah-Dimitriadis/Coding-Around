8f66aea2b5d5285b46f22c83b97b5d2dc8389fe3
thotonthepot <81646193+thotonthepot@users.noreply.github.com>
2025-01-29 19:22:07 -0500

    Removed ineligible words function working

--- Wordle/wordle.exs
+++ Wordle/wordle.exs
@@ -1,10 +1,22 @@
-# wordle pattern matching for next best guess
+# wordle pattern matching for next available words for next guess
 
 defmodule Solver do
   @words File.read!("WordBank.txt")
   |> String.split("\n", trim: true)
 
   def get_words, do: @words
+
+  def get_input do
+    input = IO.gets("Enter your last guess in the form !a?b%c where ! = green, ? = yellow, % = grey: ")
+    |> String.trim()
+    |> String.downcase()
+    if valid_input(input) do
+      input
+    else
+      IO.puts("Invalid input. Please try again.")
+      get_input()
+    end
+  end
 
   defp valid_input(input) do
     if String.length(input) == 10 and Regex.match?(~r/^([!?%][a-z]{1})+$/, input)do
@@ -14,46 +26,58 @@ defmodule Solver do
     end
   end
 
-  def get_input do
-    input = IO.gets("Enter your last guess: ")
-    |> String.trim()
-    |> String.downcase()
-    if valid_input(input) do
-      IO.puts("#{input} is valid!")
-      input
-    else
-      IO.puts("Invalid input, please enter 5 letters")
-      get_input()
-    end
-  end
-
-  def match(input) do
-    words = get_words()
-    if Enum.member?(words, input) do
-      IO.puts("true")
-    else
-      IO.puts("false")
-    end
-  end
-
   def suggest_words(pattern) do
-    guessed_letters = extract_guessed_chars(pattern)
-    @words |> Enum.filter(fn word -> match_word?(word, pattern, guessed_letters) end)
+    grey_letters = extract_grey_chars(pattern)
+    yellow_letters = extract_yellow_chars(pattern)
+    green_letters = extract_green_chars(pattern)
+    @words |> Enum.filter(fn word -> eligible_words?(word, grey_letters, yellow_letters, green_letters) end)
+    # |> check positions of green letters
+    # |> check positions of yellow letters
   end
 
-  def match_word? do
+  defp eligible_words?(word, grey_letters, yellow_letters, green_letters) do
+    word_chars = String.graphemes(word)   # split into char list
 
+    has_grey = Enum.all?(word_chars, fn char -> char in grey_letters end)
+    has_yellow = Enum.all?(yellow_letters, fn char -> char in word_chars end)
+    has_green = Enum.all?(green_letters, fn char -> char in word_chars end)
+
+    has_yellow and has_green and not has_grey
   end
 
-  def extract_guessed_chars(pattern) do
-    pattern |> String.to_charlist()
+  defp extract_grey_chars(pattern) do
+    pattern
+    |> String.to_charlist()
     |> Enum.chunk_every(2)
+    |> Enum.filter(fn [symbol, _] -> symbol == ?% end)
+    |> Enum.map(fn [_, letter] -> <<letter>> end)
+  end
+
+  defp extract_yellow_chars(pattern) do
+    pattern
+    |> String.to_charlist()
+    |> Enum.chunk_every(2)
+    |> Enum.filter(fn [symbol, _] -> symbol == ?? end)
+    |> Enum.map(fn [_, letter] -> <<letter>> end)
+  end
+
+  defp extract_green_chars(pattern) do
+    pattern
+     |> String.to_charlist()
+    |> Enum.chunk_every(2)
+    |> Enum.filter(fn [symbol, _] -> symbol == ?! end)
+    |> Enum.map(fn [_, letter] -> <<letter>> end)
   end
 
   def run do
-    get_input()
-    |> match
+    input = get_input()
+    valid_words = suggest_words(input)
+    IO.inspect(valid_words)
   end
 end
 
 Solver.run()
+
+# %f?r?e%s%h
+# %f%r%e?s?h
+# %f%r%e%s?h
